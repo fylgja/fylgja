ea31e62c4973520119dfc25ee5e408a16f63b6b1
GrimLink <sean.grimlink@gmail.com>
2022-01-12 17:00:14 +0100

    ADD: first version for aspect-ratio props

---
+++ components/aspect-ratio/index.js
@@ -0,0 +1,9 @@
+export default {
+    "ratio-box": "1/1", // Its a box
+    "ratio-landscape": "4/3", // The older default video ratio
+    "ratio-portrait": "3/4",
+    "ratio-widescreen": "16/9", // Default video ratio
+    "ratio-ultrawide": "21/9", // PC monitor
+    "ratio-superwide": "18/5", // PC Gamer monitor
+    "ratio-golden": "1.618/1", // Golden ratio (Google it mate)
+};
