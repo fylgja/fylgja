d9e0f47d4bde52ce8141828f1ec952b0e44a6303
grimlink <sean.grimlink@gmail.com>
2022-02-28 00:31:29 +0100

    DEL: animations
    Requires more thought before we add our own spin on it

--- components/animation/_build.js
+++
@@ -1,6 +0,0 @@
-import { propsBuilder } from "@fylgja/props-builder";
-import props from "./index.js";
-
-propsBuilder({ filename: "animation.scss", props });
-propsBuilder({ filename: "_helper.scss", props, varOnly: true });
-propsBuilder({ filename: "_frames.scss", props, frameOnly: true });
