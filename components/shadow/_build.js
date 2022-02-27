58d8921f3a4882218f7888b69db3be6192ee755f
grimlink <sean.grimlink@gmail.com>
2022-02-28 00:10:59 +0100

    IMP: build files

--- components/shadow/_build.js
+++ components/shadow/_build.js
@@ -2,5 +2,6 @@ import { propsBuilder } from "@fylgja/props-builder";
 import props from "./index.js";
 
 propsBuilder({ filename: "shadow.scss", props });
-propsBuilder({ filename: "_index.scss", props, varOnly: true });
+propsBuilder({ filename: "_index.scss", props });
+propsBuilder({ filename: "_helper.scss", props, varOnly: true });
 propsBuilder({ filename: "tokens.json", props, selector: "json" });
