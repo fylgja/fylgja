fdccb5fa6840638a338de1e003316d0e7e21b0fd
grimlink <sean.grimlink@gmail.com>
2022-05-28 19:45:51 +0200

    IMP: make aspect-ration ready for v1.2

---
+++ components/aspect-ratio/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+### Added
+- Support for CSS design tokens,
+  will become the default in v2 and moving the rest to a legacy import option
+
+## [1.0.0] - 2021-06-22
+Initial Release 🎉
--- components/aspect-ratio/_build.js
+++ components/aspect-ratio/_build.js
@@ -1,11 +1,8 @@
 import { propsBuilder } from "@fylgja/props-builder";
 import props from "./index.js";
 
-propsBuilder({ props, filename: "_helper.scss" }); // Will become the new index later
-propsBuilder({ props, filename: "aspect-ratio.css", props });
-propsBuilder({
-    props,
-    filename: "aspect-ratio.scss",
-    props,
-    generationSyntax: "css",
-});
+// TODO: Make this the new `_index.scss` in v2
+propsBuilder({ filename: "_helper.scss", props });
+// TODO: Make this the new `aspect-ratio.css` in v2
+propsBuilder({ filename: "props.css", props });
+propsBuilder({ filename: "props.scss", props, generationSyntax: "css" });
