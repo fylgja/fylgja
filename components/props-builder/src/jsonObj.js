db679fbd8504c2bccb118002624d2ee174a2629f
grimlink <sean.grimlink@gmail.com>
2022-06-04 23:10:09 +0200

    IMP: cleanups

--- components/props-builder/src/jsonObj.js
+++ components/props-builder/src/jsonObj.js
@@ -2,16 +2,15 @@
  * Convert Object to JSON safe Array of Objects,
  * to parse in the toTokens function
  *
- * Inspired by:
- * - https://github.com/argyleink/open-props/blob/main/build/to-json.js
- *
  * @param {Object} props
  * @returns Array
  */
 const jsonObj = (props) =>
-    Object.entries(props).reduce((bundle_entries, [key, val]) => {
-        bundle_entries.unshift([key, val]);
-        return bundle_entries;
-    }, []);
+    Object.entries(props)
+        .reduce((bundle_entries, [key, val]) => {
+            bundle_entries.unshift([key, val]);
+            return bundle_entries;
+        }, [])
+        .reverse();
 
 export default jsonObj;
