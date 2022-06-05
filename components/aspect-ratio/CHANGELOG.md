d7da3a71c9cada946cb72cd1bacff5bf5a4fdc84
grimlink <sean.grimlink@gmail.com>
2022-06-05 17:03:17 +0200

    Release v1.1.0

--- components/aspect-ratio/CHANGELOG.md
+++ components/aspect-ratio/CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [1.1.0] - 2022-06-05
 ### Added
 - Support for CSS design tokens,
   will become the default in v2 and moving the rest to a legacy import option
--- components/aspect-ratio/_build.js
+++ components/aspect-ratio/_build.js
@@ -4,5 +4,5 @@ import props from "./index.js";
 // TODO: Make this the new `_index.scss` in v2
 propsBuilder({ filename: "_helper.scss", props });
 // TODO: Make this the new `aspect-ratio.css` in v2
-propsBuilder({ filename: "aspect-new.css", props });
+propsBuilder({ filename: "props.css", props });
 propsBuilder({ filename: "_props.scss", props, generationSyntax: "css" });
