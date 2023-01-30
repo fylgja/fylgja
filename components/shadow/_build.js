f986a660bf7893750c6d96cc9ec123b4ab06c2f9
grimlink <sean.grimlink@gmail.com>
2023-01-30 23:31:07 +0100

    IMP: shadow props

--- components/shadow/_build.js
+++ components/shadow/_build.js
@@ -1,11 +1,45 @@
 // Fylgja (https://fylgja.dev)
 // Licensed under MIT Open Source
 
+import fs from "fs";
 import { propsBuilder } from "@fylgja/props-builder";
 import props, { staticShadows } from "./index.js";
 
-propsBuilder({ props, filename: "_index.scss", safeMode: false }); // the `/` is not part of a calc so we don't need to escape it
-propsBuilder({ props, filename: "_props.scss", generationSyntax: "css" });
-propsBuilder({ props, filename: "shadowdom.css", selector: ":host" });
-propsBuilder({ props: staticShadows, filename: "safe.css" });
+// For CommonJs export support
+const file = fs.createWriteStream("index.cjs");
+file.write(`// Fylgja (https://fylgja.dev)
+// Licensed under MIT Open Source
+
+const shadows = ${JSON.stringify(props, null, 4)};
+
+const staticShadows = ${JSON.stringify(staticShadows, null, 4)};
+
+module.exports = { ...shadows, staticShadows };
+`);
+file.end();
+
+// The Props 😎
+propsBuilder({
+    props,
+    filename: "_index.scss",
+    safeMode: false, // the `/` is not part of a calc so we don't need to escape it
+});
+propsBuilder({
+    props,
+    filename: "_props.scss",
+    generationSyntax: "css",
+});
 propsBuilder({ props, filename: "shadow.css" });
+propsBuilder({
+    props,
+    filename: "host.css",
+    selector: ":host",
+});
+propsBuilder({
+    props: staticShadows,
+    filename: "safe.css",
+});
+
+// postcss-jit-props syntax
+propsBuilder({ props, filename: "props.cjs" });
+propsBuilder({ props, filename: "props.js" });
--- components/shadow/index.cjs
+++ components/shadow/index.cjs
@@ -1,67 +1,42 @@
 // Fylgja (https://fylgja.dev)
 // Licensed under MIT Open Source
 
-/**
- * Converts any dynamic shadow to a static shadow.
- *
- * @param {{ [key: string | number]: string }} map - Array of shadow maps
- * @param {String} color - Color of shadow
- * @param {String} weight - Shadow weight
- * @returns {{ [key: string | number]: string }} - Array of shadow maps
- */
-function replaceShadowVars(map, color, weight) {
-    return Object.fromEntries(
-        Object.entries(map).map(([key, value]) => {
-            value = value.replaceAll("var(--shadow-color)", color);
-            value = value.replaceAll("var(--shadow-weight)", weight);
-            return [key, value];
-        })
-    );
-}
-
-const shadowUmbra =
-    "hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))";
-const shadowPenumbra =
-    "hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))";
-const shadowAmbient =
-    "hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))";
-const shadowInset =
-    "hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))";
 const shadows = {
     "shadow-color": "0 0% 50%",
     "shadow-weight": "1%",
-    shadow: {
-        1: `0 2px 1px -1px ${shadowUmbra}, 0 1px 1px ${shadowPenumbra}, 0 1px 3px ${shadowAmbient}`,
-        2: `0 3px 1px -2px ${shadowUmbra}, 0 2px 2px ${shadowPenumbra}, 0 1px 5px ${shadowAmbient}`,
-        3: `0 2px 4px -1px ${shadowUmbra}, 0 4px 5px ${shadowPenumbra}, 0 1px 10px ${shadowAmbient}`,
-        4: `0 3px 5px -1px ${shadowUmbra}, 0 6px 10px ${shadowPenumbra}, 0 1px 18px ${shadowAmbient}`,
-        5: `0 5px 5px -3px ${shadowUmbra}, 0 8px 10px 1px ${shadowPenumbra}, 0 3px 14px 2px ${shadowAmbient}`,
-        6: `0 7px 8px -4px ${shadowUmbra}, 0 12px 17px 2px ${shadowPenumbra}, 0 5px 22px 4px ${shadowAmbient}`,
+    "shadow": {
+        "1": "0 2px 1px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 1px 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 3px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
+        "2": "0 3px 1px -2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 2px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 5px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
+        "3": "0 2px 4px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 4px 5px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 10px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
+        "4": "0 3px 5px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 6px 10px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 18px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
+        "5": "0 5px 5px -3px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 8px 10px 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 3px 14px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
+        "6": "0 7px 8px -4px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 12px 17px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 5px 22px 4px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
     },
-    insetShadow: {
-        0: `inset 0 0 0 1px ${shadowInset}`,
-        1: `inset 0 1px 2px 0 ${shadowInset}`,
-        2: `inset 0 1px 4px 0 ${shadowInset}`,
-        3: `inset 0 2px 8px 0 ${shadowInset}`,
-        4: `inset 0 2px 14px 0 ${shadowInset}`,
-    },
+    "insetShadow": {
+        "0": "inset 0 0 0 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))",
+        "1": "inset 0 1px 2px 0 hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))",
+        "2": "inset 0 1px 4px 0 hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))",
+        "3": "inset 0 2px 8px 0 hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))",
+        "4": "inset 0 2px 14px 0 hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))"
+    }
 };
 
 const staticShadows = {
-    shadow: {
-        ...replaceShadowVars(
-            shadows.shadow,
-            shadows["shadow-color"],
-            shadows["shadow-weight"]
-        ),
+    "shadow": {
+        "1": "0 2px 1px -1px hsl(0 0% 50% / calc(1% + 18%)), 0 1px 1px hsl(0 0% 50% / calc(1% + 12%)), 0 1px 3px hsl(0 0% 50% / calc(1% + 10%))",
+        "2": "0 3px 1px -2px hsl(0 0% 50% / calc(1% + 18%)), 0 2px 2px hsl(0 0% 50% / calc(1% + 12%)), 0 1px 5px hsl(0 0% 50% / calc(1% + 10%))",
+        "3": "0 2px 4px -1px hsl(0 0% 50% / calc(1% + 18%)), 0 4px 5px hsl(0 0% 50% / calc(1% + 12%)), 0 1px 10px hsl(0 0% 50% / calc(1% + 10%))",
+        "4": "0 3px 5px -1px hsl(0 0% 50% / calc(1% + 18%)), 0 6px 10px hsl(0 0% 50% / calc(1% + 12%)), 0 1px 18px hsl(0 0% 50% / calc(1% + 10%))",
+        "5": "0 5px 5px -3px hsl(0 0% 50% / calc(1% + 18%)), 0 8px 10px 1px hsl(0 0% 50% / calc(1% + 12%)), 0 3px 14px 2px hsl(0 0% 50% / calc(1% + 10%))",
+        "6": "0 7px 8px -4px hsl(0 0% 50% / calc(1% + 18%)), 0 12px 17px 2px hsl(0 0% 50% / calc(1% + 12%)), 0 5px 22px 4px hsl(0 0% 50% / calc(1% + 10%))"
     },
-    insetShadow: {
-        ...replaceShadowVars(
-            shadows.insetShadow,
-            shadows["shadow-color"],
-            shadows["shadow-weight"]
-        ),
-    },
+    "insetShadow": {
+        "0": "inset 0 0 0 1px hsl(0 0% 50% / calc(1% + 9%))",
+        "1": "inset 0 1px 2px 0 hsl(0 0% 50% / calc(1% + 9%))",
+        "2": "inset 0 1px 4px 0 hsl(0 0% 50% / calc(1% + 9%))",
+        "3": "inset 0 2px 8px 0 hsl(0 0% 50% / calc(1% + 9%))",
+        "4": "inset 0 2px 14px 0 hsl(0 0% 50% / calc(1% + 9%))"
+    }
 };
 
 module.exports = { ...shadows, staticShadows };
