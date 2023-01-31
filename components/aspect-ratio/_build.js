06e67c46df340bc6a541872b55306d9e1c290b8d
grimlink <sean.grimlink@gmail.com>
2023-01-31 01:40:10 +0100

    IMP: create cjs from main index

--- components/aspect-ratio/_build.js
+++ components/aspect-ratio/_build.js
@@ -1,9 +1,17 @@
 // Fylgja (https://fylgja.dev)
 // Licensed under MIT Open Source
 
+import fs from "fs";
 import { propsBuilder } from "@fylgja/props-builder";
 import props from "./index.js";
 
+// For CommonJs export support
+const file = fs.createWriteStream("index.cjs");
+file.write(`// Fylgja (https://fylgja.dev)\n// Licensed under MIT Open Source\n
+module.exports = ${JSON.stringify(props, null, 4)};\n`);
+file.end();
+
+// The Props 😎
 // TODO: Make this the new `_index.scss` in v2
 propsBuilder({ filename: "_helper.scss", props });
 // TODO: Make this the new `aspect-ratio.css` in v2
--- components/aspect-ratio/index.cjs
+++ components/aspect-ratio/index.cjs
@@ -2,13 +2,13 @@
 // Licensed under MIT Open Source
 
 module.exports = {
-    ratio: {
-        box: 1, // Its a box
-        landscape: "4/3", // The older default video ratio
-        portrait: "3/4",
-        widescreen: "16/9", // Default video ratio
-        superwide: "21/9", // PC monitor
-        ultrawide: "18/5", // PC Gamer monitor
-        golden: "1.618/1", // Golden ratio (Not sure what this is, Google it mate)
-    },
+    "ratio": {
+        "box": 1,
+        "landscape": "4/3",
+        "portrait": "3/4",
+        "widescreen": "16/9",
+        "superwide": "21/9",
+        "ultrawide": "18/5",
+        "golden": "1.618/1"
+    }
 };
