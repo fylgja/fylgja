9b66de0e1fe9d6b3f6b3f1b698c373d43e3e20d1
grimlink <sean.grimlink@gmail.com>
2023-02-10 17:02:25 +0100

    ADD: support fir jit props

--- components/aspect-ratio/_build.js
+++ components/aspect-ratio/_build.js
@@ -11,11 +11,19 @@ file.write(`// Fylgja (https://fylgja.dev)\n// Licensed under MIT Open Source\n
 module.exports = ${JSON.stringify(props, null, 4)};\n`);
 file.end();
 
-// The Props 😎
+// Props, scss
 // TODO: Make this the new `_index.scss` in v2
 propsBuilder({ filename: "_helper.scss", props });
+propsBuilder({ filename: "_props.scss", props, generationSyntax: "css" });
+
+// Props, css
 // TODO: Make this the new `aspect-ratio.css` in v2
 propsBuilder({ filename: "props.css", props });
+
+// Props, Shadow dom
 // TODO: rename props to aspect-ratio in v2
 propsBuilder({ filename: "props.host.css", props, selector: ":host" });
-propsBuilder({ filename: "_props.scss", props, generationSyntax: "css" });
+
+// Props, jit-props, e.g. the open-props syntax
+propsBuilder({ filename: "jit-props.js", props });
+propsBuilder({ filename: "jit-props.cjs", props });
