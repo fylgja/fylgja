79437693ce80e2998be3911fbc229aa2ac413805
grimlink <sean.grimlink@gmail.com>
2023-02-12 23:00:59 +0100

    Release v1.1 for easing

--- components/easing/CHANGELOG.md
+++ components/easing/CHANGELOG.md
@@ -6,6 +6,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.0] - 2023-02-10
+### Added
+- Add jit props support
+
+### Changed
+- Officially add shadow dom support with host import
+
+### Removed
+- **Possible Breaking Change** dropped older `shadowdom.css` import,
+  use `easing.host.css` instead
+
 ## [1.0.3] - 2022-08-13
 ### Added
 - Added support for both CommonJS and ESM import syntax's
--- components/easing/_build.js
+++ components/easing/_build.js
@@ -1,10 +1,26 @@
 // Fylgja (https://fylgja.dev)
 // Licensed under MIT Open Source
 
+import fs from "fs";
 import { propsBuilder } from "@fylgja/props-builder";
 import props from "./index.js";
 
+// For CommonJs export support
+const file = fs.createWriteStream("index.cjs");
+file.write(`// Fylgja (https://fylgja.dev)\n// Licensed under MIT Open Source\n
+module.exports = ${JSON.stringify(props, null, 4)};\n`);
+file.end();
+
+// Props, scss
 propsBuilder({ props, filename: "_index.scss" });
 propsBuilder({ props, filename: "_props.scss", generationSyntax: "css" });
-propsBuilder({ props, filename: "shadowdom.css", selector: ":host" });
+
+// Props, css
 propsBuilder({ props, filename: "easing.css" });
+
+// Props, Shadow dom
+propsBuilder({ props, filename: "easing.host.css", selector: ":host" });
+
+// Props, jit-props, e.g. the open-props syntax
+propsBuilder({ filename: "jit-props.js", props });
+propsBuilder({ filename: "jit-props.cjs", props });
