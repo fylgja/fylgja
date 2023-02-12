3ba888f05acd136a0ecfe63d06271583a72c1c3f
grimlink <sean.grimlink@gmail.com>
2023-02-13 00:21:29 +0100

    IMP: simplify cjs build
    release in next release

--- components/aspect-ratio/_build.js
+++ components/aspect-ratio/_build.js
@@ -1,15 +1,11 @@
 // Fylgja (https://fylgja.dev)
 // Licensed under MIT Open Source
 
-import fs from "fs";
 import { propsBuilder } from "@fylgja/props-builder";
 import props from "./index.js";
 
 // For CommonJs export support
-const file = fs.createWriteStream("index.cjs");
-file.write(`// Fylgja (https://fylgja.dev)\n// Licensed under MIT Open Source\n
-module.exports = ${JSON.stringify(props, null, 4)};\n`);
-file.end();
+propsBuilder({ filename: "index.cjs", props });
 
 // Props, scss
 // TODO: Make this the new `_index.scss` in v2
