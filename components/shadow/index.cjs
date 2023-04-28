f52a80dc46ec5e6deca39fc65270cc09a0f58176
grimlink <sean.grimlink@gmail.com>
2023-04-28 22:31:32 +0200

    IMP: build shadows

--- components/shadow/index.cjs
+++ components/shadow/index.cjs
@@ -7,12 +7,36 @@ const shadows = {
     "shadow-color-@media:dark": "220 40% 2%",
     "shadow-weight-@media:dark": "25%",
     "shadow": {
-        "1": "0 2px 1px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 1px 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 3px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
-        "2": "0 3px 1px -2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 2px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 5px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
-        "3": "0 2px 4px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 4px 5px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 10px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
-        "4": "0 3px 5px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 6px 10px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 1px 18px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
-        "5": "0 5px 5px -3px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 8px 10px 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 3px 14px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))",
-        "6": "0 7px 8px -4px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%)), 0 12px 17px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%)), 0 5px 22px 4px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        "1": [
+            "0 2px 1px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))",
+            "0 1px 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))",
+            "0 1px 3px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        ],
+        "2": [
+            "0 3px 1px -2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))",
+            "0 2px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))",
+            "0 1px 5px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        ],
+        "3": [
+            "0 2px 4px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))",
+            "0 4px 5px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))",
+            "0 1px 10px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        ],
+        "4": [
+            "0 3px 5px -1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))",
+            "0 6px 10px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))",
+            "0 1px 18px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        ],
+        "5": [
+            "0 5px 5px -3px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))",
+            "0 8px 10px 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))",
+            "0 3px 14px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        ],
+        "6": [
+            "0 7px 8px -4px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 18%))",
+            "0 12px 17px 2px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 12%))",
+            "0 5px 22px 4px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 10%))"
+        ]
     },
     "insetShadow": {
         "0": "inset 0 0 0 1px hsl(var(--shadow-color) / calc(var(--shadow-weight) + 9%))",
@@ -25,12 +49,12 @@ const shadows = {
 
 const staticShadows = {
     "shadow": {
-        "1": "0 2px 1px -1px hsl(0 0% 50% / 19%), 0 1px 1px hsl(0 0% 50% / 13%), 0 1px 3px hsl(0 0% 50% / 11%)",
-        "2": "0 3px 1px -2px hsl(0 0% 50% / 19%), 0 2px 2px hsl(0 0% 50% / 13%), 0 1px 5px hsl(0 0% 50% / 11%)",
-        "3": "0 2px 4px -1px hsl(0 0% 50% / 19%), 0 4px 5px hsl(0 0% 50% / 13%), 0 1px 10px hsl(0 0% 50% / 11%)",
-        "4": "0 3px 5px -1px hsl(0 0% 50% / 19%), 0 6px 10px hsl(0 0% 50% / 13%), 0 1px 18px hsl(0 0% 50% / 11%)",
-        "5": "0 5px 5px -3px hsl(0 0% 50% / 19%), 0 8px 10px 1px hsl(0 0% 50% / 13%), 0 3px 14px 2px hsl(0 0% 50% / 11%)",
-        "6": "0 7px 8px -4px hsl(0 0% 50% / 19%), 0 12px 17px 2px hsl(0 0% 50% / 13%), 0 5px 22px 4px hsl(0 0% 50% / 11%)"
+        "1": "0 2px 1px -1px hsl(0 0% 50% / 19%),0 1px 1px hsl(0 0% 50% / 13%),0 1px 3px hsl(0 0% 50% / 11%)",
+        "2": "0 3px 1px -2px hsl(0 0% 50% / 19%),0 2px 2px hsl(0 0% 50% / 13%),0 1px 5px hsl(0 0% 50% / 11%)",
+        "3": "0 2px 4px -1px hsl(0 0% 50% / 19%),0 4px 5px hsl(0 0% 50% / 13%),0 1px 10px hsl(0 0% 50% / 11%)",
+        "4": "0 3px 5px -1px hsl(0 0% 50% / 19%),0 6px 10px hsl(0 0% 50% / 13%),0 1px 18px hsl(0 0% 50% / 11%)",
+        "5": "0 5px 5px -3px hsl(0 0% 50% / 19%),0 8px 10px 1px hsl(0 0% 50% / 13%),0 3px 14px 2px hsl(0 0% 50% / 11%)",
+        "6": "0 7px 8px -4px hsl(0 0% 50% / 19%),0 12px 17px 2px hsl(0 0% 50% / 13%),0 5px 22px 4px hsl(0 0% 50% / 11%)"
     },
     "insetShadow": {
         "0": "inset 0 0 0 1px hsl(0 0% 50% / 10%)",
