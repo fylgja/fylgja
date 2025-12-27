dd697b2aa601097361c08d107547e9e377fc77fe
grimlink <sean.grimlink@gmail.com>
2025-12-27 13:32:21 +0100

    IMP: test env

--- _tests/components/vite.config.js
+++ _tests/components/vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@fylgja/base": path.resolve(__dirname, "../../base"),
+			"@fylgja/badge": path.resolve(__dirname, "../../components/badge"),
 			"@fylgja/input-group": path.resolve(
 				__dirname,
 				"../../components/input-group",
 			),
+			"@fylgja/toast": path.resolve(__dirname, "../../components/toast"),
 			"@fylgja/card": path.resolve(__dirname, "../../components/card"),
 			"@fylgja/form-extend": path.resolve(
 				__dirname,
